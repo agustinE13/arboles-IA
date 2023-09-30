7da01c1e7e08b4c5fef96bd3c57066f637ab313a
Agustin Estrada <20030136@itcelaya.edu.mx>
2023-09-30 11:31:00 -0600

    comentarios de funcionamiento de la clase NodoArbol

--- src/main/java/org/example/NodoArbol.java
+++ src/main/java/org/example/NodoArbol.java
@@ -3,16 +3,20 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+// Definición de la clase NodoArbol
 class NodoArbol {
-    int val;
-    List<NodoArbol> children;
+    int val;                 // Valor almacenado en el nodo
+    List<NodoArbol> children; // Lista de nodos hijos
 
+    // Constructor de la clase que recibe un valor para inicializar el nodo
     public NodoArbol(int val) {
-        this.val = val;
-        this.children = new ArrayList<>();
+        this.val = val;         // Asigna el valor proporcionado al nodo
+        this.children = new ArrayList<>(); // Inicializa la lista de hijos como una lista vacía
     }
 
+    // Método para agregar un nodo hijo al nodo actual
     public void agregarNodo(NodoArbol child) {
-        children.add(child);
+        children.add(child); // Agrega el nodo child a la lista de hijos del nodo actual
     }
 }
+
