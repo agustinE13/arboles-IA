54b08c6da56d62949d1405d2a9f36fd8f4d61ef3
Agustin Estrada <20030136@itcelaya.edu.mx>
2023-09-30 11:34:18 -0600

    comentarios de funcionamiento de la clase ArbolBinario

--- src/main/java/org/example/ArbolBinario.java
+++ src/main/java/org/example/ArbolBinario.java
@@ -4,48 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+// Definición de la clase ArbolBinario
 class ArbolBinario {
+
+    // Método para calcular la suma máxima de un árbol
     public static int sumaMaxima(NodoArbol raiz) {
         if (raiz == null) {
-            return 0;
+            return 0; // Si la raíz es nula, la suma máxima es 0
         }
 
-        Stack<NodoArbol> stack = new Stack<>();
-        Stack<Integer> sumStack = new Stack<>();
-        int maxSum = Integer.MIN_VALUE;
+        Stack<NodoArbol> stack = new Stack<>(); // Pila para recorrer el árbol
+        Stack<Integer> sumStack = new Stack<>(); // Pila para llevar un seguimiento de la suma actual
+        int maxSum = Integer.MIN_VALUE; // Variable para almacenar la suma máxima encontrada
 
-        stack.push(raiz);
-        sumStack.push(raiz.val);
+        stack.push(raiz); // Inicializa la pila con la raíz del árbol
+        sumStack.push(raiz.val); // Inicializa la pila de suma con el valor de la raíz
 
         while (!stack.isEmpty()) {
-            NodoArbol nodo = stack.pop();
-            int currentSum = sumStack.pop();
+            NodoArbol nodo = stack.pop(); // Obtiene el nodo actual
+            int currentSum = sumStack.pop(); // Obtiene la suma actual
 
             if (nodo.children.isEmpty()) {
-                // Leaf node, check if it has the maximum sum
+                // Nodo hoja, verifica si tiene la suma máxima
                 maxSum = Math.max(maxSum, currentSum);
             }
 
+            // Agrega los nodos hijos a la pila para procesarlos
             for (NodoArbol child : nodo.children) {
                 stack.push(child);
-                sumStack.push(currentSum + child.val);
+                sumStack.push(currentSum + child.val); // Actualiza la suma actual con el valor del hijo
             }
         }
 
-        return maxSum;
+        return maxSum; // Devuelve la suma máxima encontrada en el árbol
     }
 
+    // Método para mostrar el árbol en formato de árbol
     public static void mostrarArbol(NodoArbol raiz, String prefix, boolean rama) {
         if (raiz != null) {
-            System.out.println(prefix + (rama ? "└── " : "├── ") + raiz.val);
+            System.out.println(prefix + (rama ? "└── " : "├── ") + raiz.val); // Imprime el valor del nodo con prefijo
             List<NodoArbol> children = raiz.children;
             for (int i = 0; i < children.size() - 1; i++) {
-                mostrarArbol(children.get(i), prefix + (rama ? "    " : "│   "), false);
+                mostrarArbol(children.get(i), prefix + (rama ? "    " : "│   "), false); // Llama recursivamente para mostrar los hijos
             }
             if (children.size() > 0) {
-                mostrarArbol(children.get(children.size() - 1), prefix + (rama ? "    " : "│   "), true);
+                mostrarArbol(children.get(children.size() - 1), prefix + (rama ? "    " : "│   "), true); // Llama recursivamente para mostrar el último hijo
             }
         }
     }
-
 }
